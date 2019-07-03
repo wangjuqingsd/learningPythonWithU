7d14bb102596419d268117f5f38ec93439e5539d
lolinovsky <124450823@qq.com>
2019-07-03 16:28:38 +0800

    补上昨天作业 环境安装 py3.7

---
+++ 0-hello-world.py
@@ -0,0 +1,2 @@
+# this is my first code by python
+print('hello world')
---
+++ 1-get-user-input.py
@@ -0,0 +1,5 @@
+import math
+x = input("what r you doing? give me a number ")
+x = float(x)
+x = math.floor(x)
+print(str(x))
